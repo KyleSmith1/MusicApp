55d96d830ddd85db11fe037a7e25a8a9fce6c147
KyleSmith1 <34370230+KyleSmith1@users.noreply.github.com>
2018-11-15 21:59:45 +0000

    Add files via upload

---
+++ MiniMiniMusicApp.java
@@ -0,0 +1,60 @@
+package miniminimusicapp;
+
+import javax.sound.midi.*;
+
+/**
+ * @author 14001835
+ */
+public class MiniMiniMusicApp {
+
+    public static void main(String[] args) {
+        MiniMiniMusicApp mini = new MiniMiniMusicApp();
+        //Checks if instrument and note arguments have been included
+        if (args.length < 2) {
+            System.out.println("Don't forget the instrument and note args");
+        } else {
+            int instrument = Integer.parseInt(args[0]);
+            int note = Integer.parseInt(args[1]);
+            mini.play(instrument, note);
+        }
+    }
+
+    public void play(int instrument, int note) {
+
+        try {
+            Sequencer player = MidiSystem.getSequencer();
+            player.open();
+            Sequence seq = new Sequence(Sequence.PPQ, 4);
+            Track track = seq.createTrack();
+
+            MidiEvent event = null;
+
+            //Creates a new short message named "first"
+            ShortMessage first = new ShortMessage();
+            first.setMessage(192, 1, instrument, 0);
+            MidiEvent changeInstrument = new MidiEvent(first, 1);
+            track.add(changeInstrument);
+            
+            //Creates a new short message named "a"
+            ShortMessage a = new ShortMessage();
+            a.setMessage(144, 1, 144, 100);
+            MidiEvent noteOn = new MidiEvent(a, 1);
+            track.add(noteOn);
+            
+            //Creates a new short message named "b"
+            ShortMessage b = new ShortMessage();
+            b.setMessage(128, 1, 44, 100);
+            MidiEvent noteOff = new MidiEvent(b, 16);
+            track.add(noteOff);
+
+            player.setSequence(seq);
+
+            //Starts
+            player.start();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+}
